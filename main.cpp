8b815b553d8523612741fc545923646d5f045fe9
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-08-24 15:41:25 +0500

    Added a function that will initiate git in the newly formed directory and will also commit the gitignore file as an initial commit.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 void createDir(std::string, std::string);
 void createGitIgnore(std::string, std::string);
-// void initiateGit();
+void initiateGit(std::string, std::string);
 
 
 int main(int argc, char *argv[]){
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]){
     std::string dirName = argv[2];
     createDir(dirName, path);
     createGitIgnore(dirName, path);
-    // initiateGit();
+    initiateGit(dirName, path);
     return 0;
 }
 
@@ -23,5 +23,10 @@ void createDir(std::string name, std::string path){
 
 void createGitIgnore(std::string name, std::string path){
     std::string command = "cd " + path + "; " + "cd " + name + "; " + "touch .gitignore";
+    system(command.c_str());
+}
+
+void initiateGit(std::string name, std::string path){
+    std::string command = "cd " + path + "; " + "cd " + name + "; " + "git init; git add -A; git commit -m \"Initial commit with             gitignore\"";
     system(command.c_str());
 }
